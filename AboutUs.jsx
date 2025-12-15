75f953fabe7a5016eb37d5fb445463ac34bf3d4d
Manu Yadav <119278894+raomanu22@users.noreply.github.com>
2025-12-15 20:22:09 +0530

    Add AboutUs component with nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>Paradise Nursery offers a wide variety of indoor and outdoor plants to brighten your home and garden.</p>
+      <p>Our mission is to bring greenery and freshness into every home with healthy, high-quality plants.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
